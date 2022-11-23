5a676d989a87cddf3c2622f1539ba360d2ca7bdf
willjos <111782618+willjos@users.noreply.github.com>
2022-11-23 17:37:23 +0000

    Add files via upload

---
+++ news_scraper/static/index.js
@@ -0,0 +1,78 @@
+window.onload = async function load() {
+  getStories()
+  document
+    .getElementById('submit-search')
+    .addEventListener('click', onSearchClick)
+}
+
+function onSearchClick() {
+  const searchInputText = document.getElementById('search').value
+
+  return
+}
+
+async function getStories() {
+  const window_url = window.location.href
+  const res = await fetch(`/stories`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+  const data = await res.json()
+
+  displayStories(data.stories)
+}
+
+async function handleVote(e) {
+  const elemID = e.target.id.split('-')
+  const id = elemID[0]
+  const direction = elemID[1]
+  const window_url = window.location.href
+  const rawRes = await fetch(`/stories/${id}/votes`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ direction }),
+    credentials: 'include'
+  })
+  // const res = await rawRes.json() //commented this out and now it auto-reloads...
+  location.reload()
+}
+
+function displayStories(stories) {
+  stories.forEach(createStory)
+}
+
+function createStory(story) {
+  const stories = document.getElementById('stories')
+  const storyWrapper = document.createElement('div')
+  let pluralS = ''
+  if (story.score !== 1) {
+    pluralS = 's'
+  }
+  if (story.score === null) {
+    story.score = 0
+  }
+  storyWrapper.innerHTML = `
+  <div>
+  <hr />
+	<p>
+		<a class="title" href=${story.url}>${story.title} </a>
+		<span>(${story.score} point${pluralS})</span>
+  
+	</p>
+  </div>`
+
+  const upvoteButton = createVoteButton('upvote', `${story.id}-up`, '⬆')
+  const downvoteButton = createVoteButton('downvote', `${story.id}-down`, '⬇')
+
+  storyWrapper.append(upvoteButton, downvoteButton)
+  stories.append(storyWrapper)
+}
+
+function createVoteButton(className, id, text) {
+  const button = document.createElement('button')
+  button.id = id
+  button.className = className
+  button.addEventListener('click', handleVote)
+  button.innerText = text
+  return button
+}
